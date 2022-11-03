1e21169fd14d92668ae52531569bc104caf4fc3f
JubayerDev <ajashes858@gmail.com>
2022-11-03 20:09:52 +0600

    name-pic-date and view added in news card

--- js/app.js
+++ js/app.js
@@ -55,7 +55,20 @@ const displayNewsCategory = (newses, CategoryName) => {
         <div class="flex flex-col p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">${news.title}</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${news.details.slice(0, 300) +
-      '...'}</p>
+    '...'}</p>
+      <div class="flex items-center justify-between mt-10">
+          <div class="flex items-center justify-evenly w-[20%]">
+            <img class="w-10 h-10 rounded-full" src="${news.author.img}" alt="" />
+            <span class="text-gray-600 font-semibold"><p>${news.author.name}</p> <p>${news.author.published_date}</p></span>
+          </div>
+          <div class="flex items-center">
+          <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+          <span class="font-semibold text-gray-600">${news.total_view}</span>
+          </div>
+          <div>
+          <svg class="w-6 h-6 text-[#003554] cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </div>
+        </div>
     </div>
     `
     newsContainer.appendChild(div);
