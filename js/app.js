f81d863caa7a03c613ad32538d274a636bbaa09c
JubayerDev <ajashes858@gmail.com>
2022-11-04 21:13:03 +0600

    responsive done

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ const displayCategoryData = (newses) => {
       "py-3",
       "px-2",
       "rounded-lg",
-      "duration-200"
+      "duration-200",
+      "block",
     );
     li.innerHTML = `
         <button onclick="loadNewsCategory('${news.category_id}','${news.category_name}')"
@@ -94,7 +95,7 @@ const displayNewsCategory = (newses, CategoryName) => {
           <div class="flex items-center">
           <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
           <span class="font-semibold text-gray-600">${
-            news.total_view ? news.total_view : "No Views"
+            news.total_view ? news.total_view + 'M' : "No Views"
           }</span>
           </div>
           <div>
