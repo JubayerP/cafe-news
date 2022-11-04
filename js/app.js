5db9fbe752ca4ad90d3ed411da2668b2227dc737
JubayerDev <ajashes858@gmail.com>
2022-11-04 21:38:37 +0600

    some error fixed

--- js/app.js
+++ js/app.js
@@ -157,7 +157,7 @@ const displayNewsDetails = (news) => {
       <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
       <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
     </svg>
-      <p class="font-semibold mr-2">${news.total_view + 'M'}</p>
+      <p class="font-semibold mr-2">${news.total_view ? news.total_view + 'M' : 'No Views'}</p>
       </div>
 
       <div class="flex items-center">
@@ -172,7 +172,7 @@ const displayNewsDetails = (news) => {
       <div class="flex flex-row justify-around items-center mt-5">
           <div class="flex flex-col justify-center items-center">
           <img class="w-[40px] h-[40px] rounded-full" src="${news.author.img}" alt="" />
-          <p class="font-semibold">${news.author.name}</p>
+          <p class="font-semibold">${news.author.name ? news.author.name : 'No Name'}</p>
           </div>
           <div class="flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
